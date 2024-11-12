8ef4575307081f5005fdfa5585adee134269ab97
AlexTheWhiteHat <131209543+alex-aviles@users.noreply.github.com>
2024-11-12 09:55:24 -0700

    Just clarifying the structure

--- operator/main.py
+++ operator/main.py
@@ -1,6 +1,9 @@
 import Eshu
-from Eshu.c2 import msf, sliver
+from Eshu import frontend
+from Eshu.c2 import msf, sliver, backend
 
+frontend.Frontend()
+backend.Backend()
 msfInstance = msf.metasploit.Metasploit()
 sliverInstance = sliver.bfsliver.Sliver()
 
